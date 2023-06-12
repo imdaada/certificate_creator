a5ad4fa5dc4642d9c8b1478226becffe1de5300b
imdaada <49760536+imdaada@users.noreply.github.com>
2023-06-12 16:52:37 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+import docx
+from docx2pdf import convert
+
+doc = docx.Document('example.docx')
+
+for paragraph in doc.paragraphs:
+    for run in paragraph.runs:
+        if run.text == "??":
+            names = open('names.txt', encoding='utf-8')
+            run = run.clear()
+            inst = "dssd"
+            counter = 0
+            for name in names:
+                name = name.strip()
+                print(name)
+                run.add_text(name)
+                doc.save(f'{name}.docx')
+                convert(f"{name}.docx")
+                counter += 1
+            print(f"Созданы {counter} грамоты")
+            names.close()
