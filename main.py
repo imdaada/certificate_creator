3a6427dcff6f2eab1fbcacf11e29f6be44a54e02
imdaada <49760536+imdaada@users.noreply.github.com>
2023-06-12 17:20:24 +0300

    fix

--- main.py
+++ main.py
@@ -2,21 +2,21 @@ import docx
 from docx2pdf import convert
 import os
 
-doc = docx.Document('example.docx')
-
+doc = docx.Document('input/example.docx')
+os.mkdir("certificates")
 for paragraph in doc.paragraphs:
     for run in paragraph.runs:
         if run.text == "??":
-            names = open('names.txt', encoding='utf-8')
+            names = open('input/names.txt', encoding='utf-8')
             run = run.clear()
             counter = 0
             for name in names:
                 name = name.strip()
                 print(name)
                 run.add_text(name)
-                doc.save(f'{name}.docx')
-                convert(f"{name}.docx")
-                os.remove(f'{name}.docx')
+                doc.save(f'certificates/{name}.docx')
+                convert(f"certificates/{name}.docx")
+                os.remove(f'certificates/{name}.docx')
                 counter += 1
             print(f"Созданы {counter} грамоты")
             names.close()
